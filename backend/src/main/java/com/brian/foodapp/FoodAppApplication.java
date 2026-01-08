fc907ef4c949d714d5f7c91fff4a6b535f9cc3dd
brianzhang8 <baisheng.zhang2000@gmail.com>
2026-01-07 20:28:18 -0500

    test for actions

--- backend/src/main/java/com/brian/foodapp/FoodAppApplication.java
+++ backend/src/main/java/com/brian/foodapp/FoodAppApplication.java
@@ -11,4 +11,9 @@ public class FoodAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodAppApplication.class, args);
     }
+
+    public static String getName(){
+        return "user";
+    }
+
 }
